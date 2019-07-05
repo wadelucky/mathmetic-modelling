ab90bdaad931662bb43bc271cdcc97be8d29e5ec
wadelucky <44307100+wadelucky@users.noreply.github.com>
2019-07-05 14:59:37 +0800

    更新处理标准分和原始分下两种评判专家（作品）结果

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@ int main()
 {
     ifstream in;
     in.open("data_ori.txt");//only considering original score now
-    //in.open("data_std.txt");//now is the previous std score
-    //in.open("data_new_std.txt");//now is the updated std score
+    ofstream out;
+    out.open("new_ori(评判系数).txt");
     double dat[1050][6];
-    //double exp_ori[46];
-    //double exp_std[46];
     double full[1050][46];
     double avg_ori[1050];
     int people=0;
+    double work_revised[1050];
+    double ref_weight[46];
+    double sum_weight=0;
+    double temp[46];
+    //double weight[1050][46];
     for (int i=1;i<=1046;i++){
         for (int j=0;j<6;j++){
             in>>dat[i][j];
@@ -28,12 +31,6 @@ int main()
         avg_ori[i]=avg_ori[i]/3;//adjust average
     }
 
-    ofstream out;
-    out.open("new_ranking.txt");
-    //double temp[46];
-    //double weight[1050][46];
-    double ref_weight[46];
-    double sum_weight=0;
     for(int i=1;i<=1046;i++){
         for(int j=1;j<=45;j++){
             if(full[i][j]!=0){
@@ -44,13 +41,15 @@ int main()
         }
     }
     //cout<<"sum_weight "<<sum_weight<<endl;
+    out<<"45位专家根据原始分权重情况(顺序)"<<endl;
     for(int j=1;j<=45;j++){
         //cout<<"ref_weight "<<ref_weight[j]<<endl;
         ref_weight[j]=ref_weight[j]/sum_weight;
-        cout<<ref_weight[j]<<endl;
+        out<<ref_weight[j]<<endl;
     }
 
-    double work_revised[1050];
+    out<<"1046个作品根据原始分加权后情况(顺序)"<<endl;
+
     for(int i=1;i<=1046;i++){
         double temp_sum=0;//calculating current weight sum for these 3 out of 45 referees
         for(int j=1;j<=45;j++){
@@ -61,17 +60,93 @@ int main()
         }
         out<<work_revised[i]/temp_sum<<endl;
     }
+    out.close();
 
-//    for (int j=1;j<=45;j++){
-//        for(int i=1;i<=1046;i++){
-//            if(full[i][j]!=0){//not zero
-//                temp[j]+=(avg_ori[i]-full[i][j])*(avg_ori[i]-full[i][j]);
-//            }
-//        }
-//        temp[j]=sqrt(temp[j]);
-//        out<<temp[j]<<endl;
-//    }
+    out.open("new_ori(欧氏距离).txt");
+    out<<"45位专家根据原始分欧式距离(顺序)"<<endl;
+    for (int j=1;j<=45;j++){
+        for(int i=1;i<=1046;i++){
+            if(full[i][j]!=0){//not zero
+                temp[j]+=(avg_ori[i]-full[i][j])*(avg_ori[i]-full[i][j]);
+            }
+        }
+        temp[j]=sqrt(temp[j]);
+        out<<temp[j]<<endl;
+    }
+    out.close();
 
-    //cout << "Hello world!" << endl;
+    out.open("new_std(评判系数).txt");
+    in.open("data_std.txt");//only considering std score now
+    sum_weight=0;//renew
+    for (int i=1;i<=1046;i++){//renew
+            avg_ori[i]=0;
+            work_revised[i]=0;
+    }
+    for (int j=1;j<=45;j++)ref_weight[j]=0;//renew
+    for (int i=1;i<=1046;i++){
+        for (int j=0;j<6;j++){
+            in>>dat[i][j];
+            //cout<<dat[i][j]<<endl;
+        }
+        for (int j=0;j<6;j=j+2){
+            //cout<<dat[i][j]<<endl;
+            people=dat[i][j];
+            full[i][people]=dat[i][j+1];//full[i][j] is work i's score given by expert j
+            avg_ori[i]+=dat[i][j+1];
+        }
+        avg_ori[i]=avg_ori[i]/3;//adjust average
+    }
+
+
+    //out.open("new_std.txt");
+    //double temp[46];
+    //double weight[1050][46];
+
+    for(int i=1;i<=1046;i++){
+        for(int j=1;j<=45;j++){
+            if(full[i][j]!=0){
+                //weight[i][j]=1/(1+(full[i][j]-avg_ori[i])*(full[i][j]-avg_ori[i]));
+                ref_weight[j]+=1/(1+(full[i][j]-avg_ori[i])*(full[i][j]-avg_ori[i]));
+                sum_weight+=1/(1+(full[i][j]-avg_ori[i])*(full[i][j]-avg_ori[i]));
+            }
+        }
+    }
+    //cout<<"sum_weight "<<sum_weight<<endl;
+    out<<"45位专家根据标准分权重情况(顺序)"<<endl;
+    for(int j=1;j<=45;j++){
+        //cout<<"ref_weight "<<ref_weight[j]<<endl;
+        ref_weight[j]=ref_weight[j]/sum_weight;
+        out<<ref_weight[j]<<endl;
+    }
+
+    out<<"1046个作品根据标准分加权后情况(顺序)"<<endl;
+
+    for(int i=1;i<=1046;i++){
+        double temp_sum=0;//calculating current weight sum for these 3 out of 45 referees
+        for(int j=1;j<=45;j++){
+            work_revised[i]+=ref_weight[j]*full[i][j];
+            if(full[i][j]!=0){
+                temp_sum+=ref_weight[j];
+            }
+        }
+        out<<work_revised[i]/temp_sum<<endl;
+    }
+    out.close();
+
+    out.open("new_std(欧氏距离).txt");
+    out<<"45位专家根据标准分欧式距离(顺序)"<<endl;
+    for (int j=1;j<=45;j++){
+        for(int i=1;i<=1046;i++){
+            if(full[i][j]!=0){//not zero
+                temp[j]+=(avg_ori[i]-full[i][j])*(avg_ori[i]-full[i][j]);
+            }
+        }
+        temp[j]=sqrt(temp[j]);
+        out<<temp[j]<<endl;
+    }
+    out.close();
+
+
     return 0;
 }
+
